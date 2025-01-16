38f036652e48970f225e798b6b4a3b0cc0ab890f
Eugene-113 <eugene.yeee@gmail.com>
2025-01-16 20:53:46 +0900

    feature: 머터리얼 라이브러리 추가, 메인화면 구성

--- Doraboda/app/build.gradle.kts
+++ Doraboda/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.univ.doraboda"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.univ.doraboda"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -45,5 +45,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation ("com.jakewharton.timber:timber:5.0.1")
-    implementation ("androidx.viewpager2:viewpager2:1.1.0")
+    implementation ("com.google.android.material:material:1.9.0")
 }
--- Doraboda/app/src/main/java/com/univ/doraboda/MainActivity.kt
+++ Doraboda/app/src/main/java/com/univ/doraboda/MainActivity.kt
@@ -2,10 +2,27 @@ package com.univ.doraboda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import java.util.Objects
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tabLayout: TabLayout = findViewById(R.id.homeTabLayout)
+        val homeAdapter: ViewPager2 = findViewById(R.id.homeViewPager)
+        homeAdapter.adapter = HomeViewPager2Adapter(this)
+
+        TabLayoutMediator(
+            tabLayout, homeAdapter
+        ) { tab, position ->
+            tab.text = when(position){
+                0 -> "소리"
+                1 -> "일기"
+                else -> "설정"
+            }
+        }.attach()
     }
 }
