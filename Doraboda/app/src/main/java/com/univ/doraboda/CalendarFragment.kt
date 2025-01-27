d1e7c98e990f024d8e2c114cb2eb6458015b647f
Eugene-113 <eugene.yeee@gmail.com>
2025-01-27 22:32:37 +0900

    feature: CalendarFragment에 SettingsActivity 실행되는 버튼 생성, xml 파일 오타 수정

--- Doraboda/app/src/main/java/com/univ/doraboda/CalendarFragment.kt
+++ Doraboda/app/src/main/java/com/univ/doraboda/CalendarFragment.kt
@@ -1,9 +1,11 @@
 package com.univ.doraboda
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.*
+import android.widget.ImageView
 
 class CalendarFragment : Fragment() {
 
@@ -17,6 +19,15 @@ class CalendarFragment : Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val intent = Intent(activity, SettingsActivity::class.java)
+        val calendarSettingImageView: ImageView = view.findViewById(R.id.calendarSettingsImageView)
+        calendarSettingImageView.setOnClickListener {
+            startActivity(intent)
+        }
     }
 
     override fun onAttach(context: Context) {
