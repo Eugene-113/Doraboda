1a24a409b64286a827ad91fbdb39ea74c5c6a7fc
Eugene-113 <eugene.yeee@gmail.com>
2025-02-10 20:22:13 +0900

    refactor: DataBinding을 Calendarfragment에 적용

--- Doraboda/app/src/main/java/com/univ/doraboda/CalendarFragment.kt
+++ Doraboda/app/src/main/java/com/univ/doraboda/CalendarFragment.kt
@@ -5,28 +5,24 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.*
-import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
+import com.univ.doraboda.databinding.FragmentCalendarBinding
 
 class CalendarFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        val binding: FragmentCalendarBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_calendar, container, false)
+        val intent = Intent(activity, SettingsActivity::class.java)
+        binding.calendarSettingsImageView.setOnClickListener {
+            startActivity(intent)
+        }
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val intent = Intent(activity, SettingsActivity::class.java)
-        val calendarSettingImageView: ImageView = view.findViewById(R.id.calendarSettingsImageView)
-        calendarSettingImageView.setOnClickListener {
-            startActivity(intent)
-        }
     }
 
     override fun onAttach(context: Context) {
--- Doraboda/app/src/main/java/com/univ/doraboda/CalendarItem.kt
+++ Doraboda/app/src/main/java/com/univ/doraboda/CalendarItem.kt
@@ -1,4 +1,5 @@
 package com.univ.doraboda
 
-data class CalendarItem(val year: Int, val month: Int, val days: ArrayList<DayItem>) {
+//type: '0'-month, '1'-day, '2'-empty
+data class CalendarItem(val type: Int, val year: Int, val month: Int, val days: ArrayList<DayItem>) {
 }
