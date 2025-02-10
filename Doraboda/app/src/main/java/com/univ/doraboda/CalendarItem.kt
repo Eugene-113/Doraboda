96f06e91bfcaa71bcf656adafefd7ba858831112
Eugene-113 <eugene.yeee@gmail.com>
2025-02-10 22:28:21 +0900

    feature: 캘린더 생성을 위한 CalendarUtil 추가

--- Doraboda/app/src/main/java/com/univ/doraboda/CalendarItem.kt
+++ Doraboda/app/src/main/java/com/univ/doraboda/CalendarItem.kt
@@ -1,5 +1,4 @@
 package com.univ.doraboda
 
-//type: '0'-month, '1'-day, '2'-empty
-data class CalendarItem(val type: Int, val year: Int, val month: Int, val days: ArrayList<DayItem>) {
+data class CalendarItem(val year: Int, val month: Int, val days: ArrayList<DayItem>) {
 }
---
+++ Doraboda/app/src/main/java/com/univ/doraboda/CalendarUtil.kt
@@ -0,0 +1,23 @@
+package com.univ.doraboda
+
+import java.util.Calendar
+
+class CalendarUtil {
+    fun getDays(cal: Calendar): ArrayList<CalendarItem>{
+        val calendarList = ArrayList<CalendarItem>()
+        for(i in -1..1){
+            val calendar = Calendar.getInstance()
+            calendar.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + i, 1, 0, 0, 0)
+            val dayList = ArrayList<DayItem>()
+            for(j in 0..<calendar.get(Calendar.DAY_OF_WEEK)-1){
+                dayList.add(DayItem(0))
+            }
+            for(j in 1..calendar.getActualMaximum(Calendar.DATE)){
+                val dayItem = DayItem(j)
+                dayList.add(dayItem)
+            }
+            calendarList.add(CalendarItem(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)+1, dayList))
+        }
+        return calendarList
+    }
+}
